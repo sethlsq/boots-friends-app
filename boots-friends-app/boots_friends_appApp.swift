2a2ad649e68584905938c96949961a250ef757a5
mudd258 <81851831+mudd258@users.noreply.github.com>
2022-07-09 10:52:32 +0800

    Initial Commit

---
+++ boots-friends-app/boots_friends_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  boots_friends_appApp.swift
+//  boots-friends-app
+//
+//  Created by Seth Loh on 9/7/22.
+//
+
+import SwiftUI
+
+@main
+struct boots_friends_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
